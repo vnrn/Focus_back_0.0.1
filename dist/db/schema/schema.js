0a7ce86c9a0d8aaed93c64faa1ba20c1c5bcdac9
Kaniel Outis <vnun.b2b@gmail.com>
2024-11-25 21:10:35 +0300

    Delete dist directory

--- dist/db/schema/schema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Analytics = void 0;
-const pg_core_1 = require("drizzle-orm/pg-core");
-// main schema structure
-const Focus = (0, pg_core_1.pgSchema)("Focus");
-exports.Analytics = (0, pg_core_1.pgSchema)("Analytics");
-exports.default = Focus;
