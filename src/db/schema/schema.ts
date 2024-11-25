fc353e28419e8a4c810e5e2b28c2614e1a5d84ea
Kaniel Outis <vnun.b2b@gmail.com>
2024-11-25 21:10:44 +0300

    Delete src directory

--- src/db/schema/schema.ts
+++
@@ -1,7 +0,0 @@
-import { pgSchema } from "drizzle-orm/pg-core";
-
-// main schema structure
-const Focus = pgSchema("Focus");
-export const Analytics = pgSchema("Analytics");
-
-export default Focus;
